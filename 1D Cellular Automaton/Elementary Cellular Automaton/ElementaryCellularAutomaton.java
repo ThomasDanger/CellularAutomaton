47e8bc7e5c40e9e560536d6c7bdc8ca242cc85bf
Thomas <thomasconnerse@gmail.com>
2023-02-04 23:01:24 -0600

    Create ElementaryCellularAutomaton.java

---
+++ 1D Cellular Automaton/Elementary Cellular Automaton/ElementaryCellularAutomaton.java
@@ -0,0 +1,77 @@
+// Created by: Thomas Conner
+
+/* 
+Elementary Cellular Automaton created without using arrays
+This program is incomplete as I will stil need to implement "wrapping" using arithmetic operations
+FOR STUDENTS:
+If you are a student, do NOT copy and paste this code on a programming assignment. Your
+teacher/professor will not expect you to use bitwise functions like this.
+YOU WILL GET CAUGHT CHEATING
+*/
+
+package com.mycompany.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        //Probably not the best way to perform rudimentary input validation, but input validation
+        //was sort of an after-thought with this program
+        
+        int rule = -1;
+        while(rule < 0 || rule > 255){
+            System.out.print("Enter rule (0-255): ");
+            rule = scan.nextInt();
+        }
+        
+        int numGens = -1;
+        while(numGens<=0){
+            System.out.print("Enter num generations: ");
+            numGens = scan.nextInt();
+        }
+        
+        int cur = 1<<16; //This makes it easier to change starting pos intead of putting an int literal
+        drawRow(cur);
+        for(int i = 0; i < numGens; i++){
+            cur = calcNextRow(cur, rule);
+            drawRow(cur);
+        }
+        scan.close();
+    }
+    
+    //Calculates the binary string representing the next row in the cellular automaton
+    
+    public static int calcNextRow(int prev, int rule){
+        int cur = 0;
+        
+        if((rule&(1<<(((prev&(3<<30))>>>30) + ((prev&1)<<2)))) !=0){
+            cur++;
+        }
+        
+        for(int i = 0; i < 31; i++){
+            cur=cur<<1;
+            if((rule&(1<<((prev&(7<<29))>>>29)))!=0)
+                cur++;
+            prev=(prev<<1)+((prev&(1<<31))>>>31);
+        }
+        
+        return cur;
+    }
+    
+    //Draws the given bit string in ascii characters
+    
+    public static void drawRow(int row){
+        for(int i = 31; i >=0; i--){
+            if((row&(1<<i)) == 1<<i){
+                System.out.print("$");
+            }
+            else{
+                System.out.print(".");
+            }
+        }
+        System.out.println();
+    }
+}
